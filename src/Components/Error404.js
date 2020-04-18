4310e37c5c295520047c6f7b4498c39ebdddc203
Aldo Escudero <ajescudero@gmail.com>
2020-04-18 20:46:44 -0300

    Remove conditions component

--- src/Components/Error404.js
+++ src/Components/Error404.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from "@material-ui/icons/Home";
 
 const Error404 = () => {
-    return (
-        <div>
-            Error 404
-        </div>
-    )
-}
+  let url = useLocation();
+
+  return (
+    <>
+      <Alert severity="error" style={{ maxWidth: 752, margin: "2rem auto 0" }}>
+        <AlertTitle>Error 404</AlertTitle>
+        <p>
+          Resource{" "}
+          <b>
+            <i>{url.pathname}</i>
+          </b>{" "}
+          not found.
+        </p>
+      </Alert>
+      <div style={{ margin: "1rem auto", textAlign: "center" }}>
+        <Link to="/">
+          <IconButton color="primary">
+            <HomeIcon />
+          </IconButton>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default Error404;
